6279318624738ca9906ce7f6f2daed2a82189bfd
Brandon Dimcheff <brandon@dimcheff.com>
2019-02-20 23:28:01 -0500

    add some randomness to the function

--- cloudfunction/function.go
+++ cloudfunction/function.go
@@ -3,6 +3,7 @@ package cloudfunction
 import (
 	"fmt"
 	"html"
+	"math/rand"
 	"net/http"
 	"time"
 )
@@ -10,7 +11,12 @@ import (
 // HelloWorld prints the JSON encoded "message" field in the body
 // of the request or "Hello, World!" if there isn't one.
 func HelloWorld(w http.ResponseWriter, r *http.Request) {
-	time.Sleep(5 * time.Second)
+	if rand.Float64() < 0.1 {
+		w.WriteHeader(500)
+		return
+	}
+
+	time.Sleep(time.Duration(rand.Int31n(10)) * time.Second)
 
 	fmt.Fprint(w, html.EscapeString("i'm slow"))
 }
