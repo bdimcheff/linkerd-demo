c33e642999e29072e131f27a61ae460e02dd5afb
Brandon Dimcheff <brandon@dimcheff.com>
2019-02-23 18:33:43 +0000

    add k8s-hosted service

---
+++ go.mod
@@ -0,0 +1,6 @@
+module github.com/bdimcheff/linkerd-3rdparty-demo
+
+require (
+	github.com/gorilla/handlers v1.4.0
+	github.com/gorilla/mux v1.7.0
+)
---
+++ service.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/gorilla/handlers"
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/", HomeHandler)
+	r.HandleFunc("/3rdparty", ThirdPartyHandler)
+	http.ListenAndServe(":8080", handlers.LoggingHandler(os.Stdout, r))
+}
+
+func HomeHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	io.WriteString(w, "hello world")
+}
+
+func ThirdPartyHandler(w http.ResponseWriter, r *http.Request) {
+	start := time.Now()
+	http.Get("http://us-central1-linkerd-external-demo.cloudfunctions.net/slow")
+	end := time.Now()
+
+	dur := end.Sub(start)
+
+	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "application/json")
+	io.WriteString(w, fmt.Sprintf(`{"duration": %d}`, dur))
+}
